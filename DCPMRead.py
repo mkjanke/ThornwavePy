913671a9205bd36d4cfd7ba34ac08fe9da02f541
--global <--global>
2022-05-29 14:51:59 -0500

    Change bluetooth library from bluepy to bluezero

--- DCPMRead.py
+++ DCPMRead.py
@@ -22,7 +22,7 @@
 #	2021-01-23	V 0.2 - Swtich from gatttool to bluepy
 #	2021-06-03	V 0.3 - Reduce precision on measurement outputs to reflect precision of devices
 #	2021-07-11	V 0.4 - round() some variables and handle '-0' special case
-# 2022-05-29  V 0.5 - Change bluetooth library from bluepy to bluezero
+#	2022-05-29  V 0.5 - Change bluetooth library from bluepy to bluezero
 #
 #	Reads characteristic 0x15 from Thornwave Bluetooth Battery Monitor 
 #	Outputs in various formats
